5be76866be59bc74c4309a85dc960aeb2e63a314
Richard Sims <9633324@student.swin.edu.au>
2018-09-20 21:39:34 +1000

    added credit and savings accounts, not finished

--- week08bankabstract/AccountLibrary/CreditAccount.cs
+++ week08bankabstract/AccountLibrary/CreditAccount.cs
@@ -1,7 +1,23 @@
 namespace AccountLibrary
 {
-    public class CreditAccount
+    public class CreditAccount : Account
     {
+        public decimal CreditLimit { get; set; }
+
+        public CreditAccount(string accountNumber, string customerName, string pin, decimal creditLimit)
+            : base(accountNumber, customerName, pin)
+        {
+            this.CreditLimit = creditLimit;
+        }
         
+        public override bool Withdrawal(decimal amount)
+        {
+            return base.Withdrawal(amount);
+        }
+
+        public override decimal BalanceInOneYear()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- week08bankabstract/AccountLibrary/SavingsAccount.cs
+++ week08bankabstract/AccountLibrary/SavingsAccount.cs
@@ -1,7 +1,18 @@
 namespace AccountLibrary
 {
-    public class SavingsAccount
+    public class SavingsAccount : Account
     {
+        public SavingsAccount(string accountNumber, string customerName, string pin)
+            : base(accountNumber, customerName, pin)
+        {
+            
+        }
         
+        public override decimal BalanceInOneYear()
+        {
+            decimal futureBalance = this.Balance + (this.Balance * 0.02m);
+
+            return futureBalance;
+        }
     }
 }
