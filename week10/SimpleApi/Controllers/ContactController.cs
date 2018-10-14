fdbe2e0c62ea18980eab5f95bb124a0768f5e498
Richard Sims <9633324@student.swin.edu.au>
2018-10-15 10:45:42 +1100

    added week10 ]

---
+++ week10/SimpleApi/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contact")]
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("YAHOOO! Sent back from Contact controller (get)");
+        }
+        [HttpPost]
+        public IActionResult Post(string name, string email, string message)
+        {
+            return Ok("Sent back from the Contact controller!! (post)");
+        }
+    }
+}
