3c0ba531de887422cc1a495bdc0a502ce181f2c0
Richard Sims <9633324@student.swin.edu.au>
2018-10-16 14:42:07 +1100

    changed api to accept json

---
+++ week10/SimpleApi/Models/PostDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApi.Models
+{
+    public struct PostDataModel
+    {
+        public string Name;
+        public string Email;
+        public string Message;
+        public PostDataModel(string name, string email, string message)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Message = message;
+        }
+    }
+}
