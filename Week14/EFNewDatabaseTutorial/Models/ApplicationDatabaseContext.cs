a99786bb55eee43bcf8ad8662388b15419c095b8
Richard Sims <9633324@student.swin.edu.au>
2018-11-12 11:22:51 +1100

    week14 added, not finished first tutorial

---
+++ Week14/EFNewDatabaseTutorial/Models/ApplicationDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFNewDatabaseTutorial.Models
+{
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public DbSet<ShoppingCartItem> ShoppingCartItem { get; set; }
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options) { }
+    }
+}
